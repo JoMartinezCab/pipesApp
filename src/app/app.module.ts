ca178bd46c2fac98d6a075b7f729f3078582ff7b
Jorge Martinez <jo.cabmartin2@gmail.com>
2023-08-26 11:45:55 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeNGConfig } from 'primeng/api';
+
+
+//Modules
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit{
+  constructor(private  primengConfig:PrimeNGConfig){}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+}
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Modules PrimeNg
+import { MenuModule } from 'primeng/menu';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MenuModule
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PrimeNgModule { }
